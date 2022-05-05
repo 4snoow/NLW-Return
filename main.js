9318c57352b8ae44943a05d023eea3bc180cda78
Gabriel Bastos <gabribs1@hotmail.com>
2022-05-05 15:30:24 -0300

    Aula 04 - Padronização dos botões, adicionouo botão para subir a página, novas linhas javascript, footer e mudança de cores

--- main.js
+++ main.js
@@ -1,26 +1,42 @@
+
+window.addEventListener('scroll',onScroll)
+
+onScroll()
 function onScroll() {
-  if(scrollY > 0){
-    navigation.classList.add('scroll')
-  } else{
-    navigation.classList.remove('scroll')
-  } 
+  showNavOnScroll()
+  showBackToTopButtonOnScroll()
 }
 
-function openMenu(){
+function showNavOnScroll() {
+  if (scrollY > 0) {
+    navigation.classList.add('scroll')
+  } else {
+    navigation.classList.remove('scroll')
+  }
+}
+
+function showBackToTopButtonOnScroll() {
+  if (scrollY > 500) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+function openMenu() {
   document.body.classList.add('menu-expanded')
 }
 
-function closeMenu(){
+function closeMenu() {
   document.body.classList.remove('menu-expanded')
 }
-
 
 //ScrollReveal lib
 
 ScrollReveal({
-  origin:'top',
-  distance:'30px',
-  duration:1000,
+  origin: 'top',
+  distance: '30px',
+  duration: 1000
 }).reveal(`
 #home, 
 #home img, 
